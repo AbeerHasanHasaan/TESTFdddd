16503c4c216746057dad9da584ade6cc157ea8c7
AbeerHasanHasaan <abeer@rockettravelsystem.com>
2018-10-19 19:33:49 +0200

    test2

--- Form1.vb
+++ Form1.vb
@@ -2,5 +2,6 @@
 
     Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
         Dim dddd = ""
+        DevExpress.XtraEditors.XtraMessageBox.Show("")
     End Sub
 End Class
